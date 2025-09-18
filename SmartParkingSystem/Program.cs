7ed0c1120256d5277eeee1ef6c6449ff863467d7
Adva Kornblau <adva5353@gmail.com>
2025-09-19 01:07:51 +0300

    added thread test

--- SmartParkingSystem/Program.cs
+++ SmartParkingSystem/Program.cs
@@ -9,9 +9,9 @@ namespace SmartParkingSystem
         {
             Console.WriteLine("====== SIMULATION OF SMART PARKING SYSTEM ======\n");
 
-            // creating tow parking lots
-            ParkingLot lotA = new ParkingLot("Lot A", 10.0, 2); // hourly price 10, capacity 2
-            ParkingLot lotB = new ParkingLot("Lot B", 15.0, 3);
+            // creating two parking lots
+            ParkingLot lotA = new ("Lot A", 10.0, 2); // hourly price 10, capacity 2
+            ParkingLot lotB = new ("Lot B", 15.0, 3);
 
             // creating vehicles
             Vehicle car1 = new Car("111-11-111");
@@ -53,12 +53,48 @@ namespace SmartParkingSystem
             // release car from Lot B
             Console.WriteLine("\n--- Exiting Lot B ---");
             double fee2 = lotB.UnparkVehicle("MOTO-001");
-            Console.WriteLine("fee for moto1: " + fee2 + "NIS");
+            if (fee2 == 0) {
+                Console.WriteLine("Vehicle 111-11-111 not found.");
+            } else {
+                Console.WriteLine("fee for moto1: " + fee2 + "NIS");
+            }
 
             // Final status
             Console.WriteLine("\n--- Final status ---");
             lotA.PrintStatus();
             lotB.PrintStatus();
+
+            
+            // Thread-safety test: concurrent park/unpark operations
+            
+            ParkingLot lotC = new ("Lot C", 15.0, 2);
+
+            Vehicle car4 = new Car("aa1-11-111");
+            Vehicle car5 = new Car("bb2-22-222");
+            Vehicle moto2 = new Motorcycle("MOTO-002");
+
+            // Three threads attempt to park simultaneously 
+            Thread t1 = new (() => lotC.ParkVehicle(car4));
+            Thread t2 = new (() => lotC.ParkVehicle(car5));
+            Thread t3 = new (() => lotC.ParkVehicle(moto2));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+
+            lotC.PrintStatus();
+
+            // release a parking spot
+            Thread t4 = new (() => lotC.UnparkVehicle("aa1-11-111"));
+            t4.Start();
+            t4.Join();
+
+            lotC.PrintStatus();
+
 
             Console.WriteLine("\n======== END SIMULATION =========");
         }
